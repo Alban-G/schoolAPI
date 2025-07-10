c6d74a8898c4542558208825c20ba61103ebc040
Alban-G <githinjialban@gmail.com>
2025-07-10 09:44:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// entry file for the Express application
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const e = require('express');
+
+// Load environment variables from .env file
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// static files middleware
+app.use("/uploads", express.static("uploads"));
+
+
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch(err => {
+        console.error('MongoDB connection error:', err);
+    });
+
+// Server listening
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
